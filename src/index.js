66d361a6b5dfc306373dd7876479613f4b0bbf48
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-07-13 23:57:28 -0300

    feat: Add implementação do item

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import createItem from "./services/item.js";
 
+const cart = [];
+
+console.log("Bem vindo ao carrinho da Shopee!");
+
+const item1 = await createItem("Hotwheels ferrari", 20.99, 1);
+const item2 = await createItem("Hotwheels lamborghini", 39.99, 3);
+
+console.log(item2.subtotal());
