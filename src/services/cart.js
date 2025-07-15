5c05f30c7ae46ce6609120142567900c716791a3
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-07-14 22:19:01 -0300

    feat: Add no carrinho + subtotal

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,10 +1,16 @@
 //quais ações meu carrinho pode fazer
 //adicionar, deletar, remover um item, calcular total
 
-async function addItem(userCart) {}
+async function addItem(userCart, item) {
+  userCart.push(item);
+}
 
 async function deleteItem(userCart, name) {}
 
 async function removeItem(userCart, index) {}
 
-async function calculateTotal(userCart) {}
+async function calculateTotal(userCart) {
+  return userCart.reduce((total, item) => total + item.subtotal(), 0);
+}
+
+export { addItem, calculateTotal, deleteItem, removeItem };
